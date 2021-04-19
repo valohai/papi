613e3867eb75cd3d1ca64595352c0ec57e4d4427
Aarni Koskela <akx@iki.fi>
2021-04-19 12:02:50 +0300

    Rename Poetry package to valohai-papi for publishing

--- setup.py
+++ setup.py
@@ -13,10 +13,10 @@ readme = ""
 
 setup(
     long_description=readme,
-    name="papi",
+    name="valohai-papi",
     version="0.1.0",
     description="Experimental imperative Valohai pipeline API",
-    python_requires="==3.*,>=3.6.0",
+    python_requires="==3.*,>=3.6.1",
     project_urls={"homepage": "https://github.com/valohai/papi"},
     author="Aarni Koskela",
     author_email="aarni@valohai.com",
@@ -29,6 +29,11 @@ setup(
         "dev": [
             "black==20.*,>=20.8.0.b1",
             "dephell==0.*,>=0.8.3",
+            "flake8==3.*,>=3.9.0",
+            "flake8-bugbear==21.*,>=21.4.3",
+            "flake8-print==4.*,>=4.0.0",
+            "flake8-return==1.*,>=1.1.2",
+            "flake8-simplify==0.*,>=0.14.0",
             "isort==5.*,>=5.8.0",
             "mypy==0.*,>=0.812.0",
             "poethepoet==0.*,>=0.10.0",
